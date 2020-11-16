d5dd99bdbe2a54abca7e038910f837d000727964
Ted Yu <yuzhihong@gmail.com>
2020-11-16 10:15:27 -0800

    [#6352] [YSQL] YBPreloadRelCache should assign missing field when attrmiss is not null

--- src/postgres/src/backend/utils/cache/relcache.c
+++ src/postgres/src/backend/utils/cache/relcache.c
@@ -1671,7 +1671,8 @@ void YBPreloadRelCache()
 			/*
 			 * Set up constraint/default info
 			 */
-			if (constr->has_not_null || ndef > 0 || relation->rd_rel->relchecks)
+			if (constr->has_not_null || ndef > 0 ||
+				attrmiss || relation->rd_rel->relchecks)
 			{
 				relation->rd_att->constr = constr;
 
